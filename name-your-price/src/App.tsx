0857832ccbee155ecb49afe5d286c6ed34ab9871
Maximiliano Ermoglio <setmaxo@gmail.com>
2022-12-04 17:11:54 +1300

    Customizing MUI Slider component

--- name-your-price/src/App.tsx
+++ name-your-price/src/App.tsx
@@ -9,10 +9,14 @@ function App() {
     <div className="App">
       <div className="Header">
         <div className="Circles">
-          <b style={{ fontSize: "xx-large" }}>Simple, traffic-basded pricing</b>
-          <p style={{ fontSize: "large", color: "grey" }}>
-            Sign-up for our 30 day trial. No credit card required.
-          </p>
+          <div className="Title">
+            <b style={{ fontSize: "xx-large" }}>
+              Simple, traffic-basded pricing
+            </b>
+            <p style={{ fontSize: "large", color: "grey" }}>
+              Sign-up for our 30 day trial. No credit card required.
+            </p>
+          </div>
         </div>
       </div>
 
--- name-your-price/src/components/PriceContainer.tsx
+++ name-your-price/src/components/PriceContainer.tsx
@@ -1,5 +1,50 @@
 import "./PriceContainer.css";
-import Slider from "@mui/material/Slider";
+import Slider, { SliderThumb } from "@mui/material/Slider";
+import { styled } from "@mui/material/styles";
+import thumbImage from "../images/icon-slider.svg";
+
+interface NYPThumbComponentProps extends React.HTMLAttributes<unknown> {}
+
+function NYPThumbComponent(props: NYPThumbComponentProps) {
+  const { children, ...other } = props;
+  return (
+    <SliderThumb {...other}>
+      {children}
+      <span className="Thumb" />
+    </SliderThumb>
+  );
+}
+
+const ligth = "hsl(174deg 77% 70%)";
+const bold = "hsl(174, 86%, 45%)";
+
+const NYPSlider = styled(Slider)(({}) => ({
+  color: `${ligth}`,
+  padding: "50px 0",
+  "& .MuiSlider-thumb": {
+    height: 35,
+    width: 35,
+    backgroundColor: `${ligth}`,
+    border: "1px solid currentColor",
+    "&:focus, &:hover, &.Mui-active, &.Mui-focusVisible": {
+      boxShadow: `5px 7px 15px 5px ${ligth}`,
+      backgroundColor: `${bold}`,
+      cursor: "grabbing",
+    },
+    backgroundImage: `url(${thumbImage})`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    cursor: "pointer",
+  },
+  "& .MuiSlider-track": {
+    height: 6,
+  },
+  "& .MuiSlider-rail": {
+    color: "lightgrey",
+    opacity: 1,
+    height: 8,
+  },
+}));
 
 function PriceContainer() {
   return (
@@ -9,11 +54,8 @@ function PriceContainer() {
           <span>100K PAGEVIEWS</span>
           <span>$16.00</span>
           <span> / month</span>
-          <Slider
-            value={
-              30
-            } /*onChange={handleChange}*/ /*disabled*/ /*color="secondary"*/
-          />
+
+          <NYPSlider slots={{ thumb: NYPThumbComponent }} defaultValue={20} />
         </div>
       </div>
     </>
